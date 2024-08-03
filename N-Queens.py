8a751c6ef4087b3d7f9a9f9ae4e7ac1c2c94d647
dhikki-study <dhikki.study@gmail.com>
2024-08-03 10:05:05 -0400

    completed backtracking-3

---
+++ N-Queens.py
@@ -0,0 +1,71 @@
+####215. Kth Largest Element in an Array#####################################################################################################
+// Time Complexity : n^n
+// Space Complexity : nXn
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : no problem
+
+// Your code here along with comments explaining your approach in three sentences only
+We have used for loop based recursion along with backtracking where we found all the possible place where queen can be placed, and stored in another array with Boolean flag.
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        result=[]
+        mat=[[False for r in range(n)] for c in range(n)]
+        self.backtrack(result,n,mat,0)
+        return result
+        
+    def backtrack(self,result,n,mat,r):
+        #base
+        if r==n:
+            l1=[]
+            print('in base')
+            #print(mat)
+            for k in range(n):
+                str1=''
+                for l in range(n):
+                    if mat[k][l]==True:
+                        str1+='Q'
+                    else:
+                        str1+='.'
+                l1.append(str1)
+            result.append(l1)
+
+            return
+
+        #logic
+        #for i in range(n):
+        for j in range(n):
+            if self.isSafe(r,j,n,mat):
+                #print('safe:',r,j)
+                mat[r][j]=True
+                #print(mat)
+                self.backtrack(result,n,mat,r+1)
+                mat[r][j]=False
+        
+                    
+
+
+    def isSafe(self,r,c,n,mat) -> bool:
+        #check vertical
+        for i in range(r):
+            #print('safe 1:',i,r,c)
+            if mat[i][c]==True:
+                #print('false')
+                return False
+
+        #check top right
+        i,j=r,c
+        while i>=0 and j<n:
+            if mat[i][j]==True:
+                return False
+            i-=1
+            j+=1
+
+
+        #check topleft
+        i,j=r,c
+        while i>=0 and j>=0:
+            if mat[i][j]==True:
+                return False
+            i-=1
+            j-=1
+        return True
---
+++ Word_Search.py
@@ -0,0 +1,58 @@
+####215. Kth Largest Element in an Array#####################################################################################################
+// Time Complexity : not able to deduce
+// Space Complexity : k
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : no problem
+
+// Your code here along with comments explaining your approach in three sentences only
+We used recursion with direction array important thing to do is to make sure the element is changed to # once it is found also backtracked if the element is not found.
+
+class Solution:
+    def __init__(self):
+        self.flag=False
+
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        print('test',ord('b')-ord('a'))
+        r=len(board)
+        c=len(board[0])
+        #l1=[]
+        dirl=[[0,1],[1,0],[0,-1],[-1,0]]
+        len1=len(word)
+        for m in range(r):
+            for n in range(c):
+                if board[m][n]==word[0]:
+                    board[m][n]='#'
+                    #print(r,c,m,n,word[0])
+                    self.backtrack(board,word,r,c,m,n,1,dirl)
+                    if self.flag:
+                        return True
+                    board[m][n]=word[0]
+                    #print('new start:',word[0],board)
+        return self.flag
+
+    def backtrack(self,board,word,r,c,rc,cc,idx,dirl):
+        #base
+        
+        if idx==len(word):
+            #print('base:',board)
+            self.flag=True
+            return
+
+
+        #logic
+        
+        for i in dirl:
+            rn,cn=rc+i[0],cc+i[1]
+            if rn>=0 and cn>=0 and rn<r and cn<c:
+                if board[rn][cn]==word[idx]:
+                    tmp=board[rn][cn]
+                    board[rn][cn]='#'
+                    #print(board)
+                    self.backtrack(board,word,r,c,rn,cn,idx+1,dirl)
+                    board[rn][cn]=tmp
+        #board[rc][cc]==word[idx]
+        #False
+
+
+
+        
